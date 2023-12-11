14e0ad0c389496d51ba2c3a6cee79d694cba77a6
Andrei Bancos <bancos.andrei.2002@gmail.com>
2023-12-11 17:53:22 +0200

    Added DatabaseConnection

---
+++ src/main/java/com/example/proiectingineriaprogramelor/DatabaseConnection.java
@@ -0,0 +1,35 @@
+package com.example.proiectingineriaprogramelor;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static DatabaseConnection instance = null;
+    private Connection connection = null;
+
+    private DatabaseConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            String url = "jdbc:mysql://localhost:3306/Medical_app";
+            String user = "root";
+            String password = "";
+
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static synchronized DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,8 @@ module com.example.proiectingineriaprogramelor {
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
+    requires mysql.connector.j;
 
     opens com.example.proiectingineriaprogramelor to javafx.fxml;
     exports com.example.proiectingineriaprogramelor;
