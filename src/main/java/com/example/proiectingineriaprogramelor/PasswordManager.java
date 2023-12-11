6596e5cf0995e057608d780aeaf0ec98196bc9d7
Tintas Gheorghe <gheorghetintas.14@gmail.com>
2023-12-11 17:55:55 +0200

    Add PasswordManager

---
+++ src/main/java/com/example/proiectingineriaprogramelor/PasswordManager.java
@@ -0,0 +1,21 @@
+package com.example.proiectingineriaprogramelor;
+
+import java.security.MessageDigest;
+public class PasswordManager {
+    public static String encryptSHA256(String plain) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(plain.getBytes("UTF-8"));
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if (hex.length() == 1) hexString.append(0);
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+}
